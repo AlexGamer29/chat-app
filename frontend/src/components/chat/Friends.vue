a8791f0fa78e2cd162d0d645d29e2a919b1d6790
[TUF] AlexGamer29 <sun123vet@gmail.com>
2023-11-25 19:18:26 +0700

    feat(Friends): add Friends component.

---
+++ frontend/src/components/chat/Friends.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { storeToRefs } from 'pinia'
+import { useAuthStore } from '../../stores'
+const authStore = useAuthStore()
+const { user } = storeToRefs(authStore)
+
+const props = defineProps(['friend'])
+</script>
+
+<template>
+  <div class="friend">
+    <div class="friend-image">
+      <div class="image">
+        <img
+          src="https://png.pngtree.com/png-clipart/20230207/original/pngtree-beauty-logo-design-png-image_8947095.png"
+          alt=""
+        />
+      </div>
+    </div>
+    <div class="friend-name-seen">
+      <div class="friend-name">
+        <h4>{{ props.friend.first_name.concat(' ', props.friend.last_name) }}</h4>
+      </div>
+    </div>
+  </div>
+</template>
