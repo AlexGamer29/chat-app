dfa59de062fb82e1fe2573c270cd2918dcae2667
[TUF] AlexGamer29 <sun123vet@gmail.com>
2023-11-25 19:11:40 +0700

    feat(MessageSend): add MessageSend component.

---
+++ frontend/src/components/chat/MessageSend.vue
@@ -0,0 +1,87 @@
+<script>
+export default {
+  data() {
+    return {
+      emojis: [
+        '😀',
+        '😃',
+        '😄',
+        '😁',
+        '😆',
+        '😅',
+        '😂',
+        '🤣',
+        '😊',
+        '😇',
+        '🙂',
+        '🙃',
+        '😉',
+        '😌',
+        '😍',
+        '😝',
+        '😜',
+        '🧐',
+        '🤓',
+        '😎',
+        '😕',
+        '🤑',
+        '🥴',
+        '😱'
+      ],
+      message: ''
+    }
+  },
+  methods: {
+    sendMessage() {
+      this.$emit('messageFromChild', this.message)
+      this.message = ''
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="message-send-section">
+    <input type="checkbox" id="emoji" />
+    <div class="file hover-attachment">
+      <div class="add-attachment">Add Attachment</div>
+      <el-icon><CirclePlus /></el-icon>
+    </div>
+
+    <div class="file hover-image">
+      <div class="add-image">Add Image</div>
+      <label for="pic">
+        <el-icon><Picture /></el-icon>
+      </label>
+    </div>
+
+    <div class="file hover-gift">
+      <div class="add-gift">Add Gift</div>
+      <el-icon><Goods /></el-icon>
+    </div>
+
+    <div class="message-type">
+      <input
+        type="text"
+        name="message"
+        id="message"
+        class="form-control"
+        placeholder="Aa"
+        v-model="message"
+      />
+      <div class="file hover-gift">
+        <label for="emoji">
+          <el-icon><Position /></el-icon>
+        </label>
+      </div>
+    </div>
+
+    <div class="file" @click="sendMessage">❤</div>
+
+    <div class="emoji-section">
+      <div class="emoji">
+        <span v-for="emoji in emojis" :key="emoji">{{ emoji }}</span>
+      </div>
+    </div>
+  </div>
+</template>
