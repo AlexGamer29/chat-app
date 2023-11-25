1f50531710e511692cba13dd8fb649e9b51d288a
[TUF] AlexGamer29 <sun123vet@gmail.com>
2023-11-25 18:38:09 +0700

    chore: add sass support.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,74 +5,14 @@ import Alert from './components/alert/Alert.vue'
 </script>
 
 <template>
-  <NavBar />
-  <Alert />
-  <header>
-    <div class="wrapper">
-      <router-view />
-    </div>
-  </header>
+  <!-- <NavBar />
+  <Alert /> -->
+  <div>
+    <router-view />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
+<style lang="scss">
+// Import the global SCSS styles
+@import '../main.scss';
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,7 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
@@ -14,6 +13,9 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 app.use(ElementPlus)
 
 app.mount('#app')
